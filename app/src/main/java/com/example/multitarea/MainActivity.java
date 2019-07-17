533505493d7f346d15aec4b61397c6883bc2152c
willmont21 <willmont66@gmail.com>
2019-07-17 15:21:52 -0500

    Version normal

--- app/src/main/java/com/example/multitarea/MainActivity.java
+++ app/src/main/java/com/example/multitarea/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.multitarea;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void cajera(View view){
+        Cliente cliente1 = new Cliente("Cliente 1", new int[] { 2, 2, 1, 5, 2, 3 });
+        Cliente cliente2 = new Cliente("Cliente 2", new int[] { 1, 3, 5, 1, 1 });
+
+        Cajera cajera1 = new Cajera("Cajera 1");
+        Cajera cajera2 = new Cajera("Cajera 2");
+
+        // Tiempo inicial de referencia
+        long initialTime = System.currentTimeMillis();
+
+        cajera1.procesarCompra(cliente1, initialTime);
+        cajera2.procesarCompra(cliente2, initialTime);
     }
 }
