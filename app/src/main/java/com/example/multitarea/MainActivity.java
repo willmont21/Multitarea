5eb4a46c02f4ea3829599010ab015281a6e7fe63
willmont21 <willmont66@gmail.com>
2019-07-17 15:48:42 -0500

    Version thread

--- app/src/main/java/com/example/multitarea/MainActivity.java
+++ app/src/main/java/com/example/multitarea/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void cajera(View view){
+    public void normal(View view){
         Cliente cliente1 = new Cliente("Cliente 1", new int[] { 2, 2, 1, 5, 2, 3 });
         Cliente cliente2 = new Cliente("Cliente 2", new int[] { 1, 3, 5, 1, 1 });
 
@@ -25,5 +25,19 @@ public class MainActivity extends AppCompatActivity {
 
         cajera1.procesarCompra(cliente1, initialTime);
         cajera2.procesarCompra(cliente2, initialTime);
+    }
+
+    public void thread(View view) {
+
+        Cliente cliente1 = new Cliente("Cliente 1", new int[] { 2, 2, 1, 5, 2, 3 });
+        Cliente cliente2 = new Cliente("Cliente 2", new int[] { 1, 3, 5, 1, 1 });
+
+        // Tiempo inicial de referencia
+        long initialTime = System.currentTimeMillis();
+        CajeraThread cajera1 = new CajeraThread("Cajera 1", cliente1, initialTime);
+        CajeraThread cajera2 = new CajeraThread("Cajera 2", cliente2, initialTime);
+
+        cajera1.start();
+        cajera2.start();
     }
 }
